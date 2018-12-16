ee30c49b78f379c042aca461950d1312dc9966f8
ElwinBran <elwinbran@gmail.com>
2018-12-16 21:41:09 +0100

    Added the recyclerview reference and set the layout mode.

--- app/src/main/java/com/github/elwinbran/bucketlist/MainActivity.java
+++ app/src/main/java/com/github/elwinbran/bucketlist/MainActivity.java
@@ -2,12 +2,18 @@ package com.github.elwinbran.bucketlist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final RecyclerView bucketListView = findViewById(R.id.recycler_view_bucket_list);
+        bucketListView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
     }
 }
