ea947ed0deec3cfa7a8bc11c4802ebdc9aa44454
ElwinBran <elwinbran@gmail.com>
2018-12-16 21:58:16 +0100

    Changed to viewholder to correctly apply domain information.

--- app/src/main/java/com/github/elwinbran/bucketlist/ItemViewHolder.java
+++ app/src/main/java/com/github/elwinbran/bucketlist/ItemViewHolder.java
@@ -3,6 +3,7 @@ package com.github.elwinbran.bucketlist;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
 
 /**
  * The bucket list item representation.
@@ -11,8 +12,19 @@ import android.view.View;
  */
 public class ItemViewHolder extends RecyclerView.ViewHolder
 {
+    final private View view;
+
     public ItemViewHolder(@NonNull View itemView)
     {
         super(itemView);
+        view = itemView;
+    }
+
+    public void setItem(BucketListItem model)
+    {
+        TextView title = itemView.findViewById(R.id.text_view_item_title);
+        TextView description = itemView.findViewById(R.id.text_view_item_description);
+        title.setText(model.title());
+        title.setText(model.description());
     }
 }
