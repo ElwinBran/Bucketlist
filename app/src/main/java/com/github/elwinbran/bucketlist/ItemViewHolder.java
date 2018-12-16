367098e3ada035c97b82cd0b2befb7f447f21675
ElwinBran <elwinbran@gmail.com>
2018-12-16 21:47:31 +0100

    Added the viewholder for bucketlist items.

---
+++ app/src/main/java/com/github/elwinbran/bucketlist/ItemViewHolder.java
@@ -0,0 +1,18 @@
+package com.github.elwinbran.bucketlist;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * The bucket list item representation.
+ *
+ * @author Elwin Slokker
+ */
+public class ItemViewHolder extends RecyclerView.ViewHolder
+{
+    public ItemViewHolder(@NonNull View itemView)
+    {
+        super(itemView);
+    }
+}
