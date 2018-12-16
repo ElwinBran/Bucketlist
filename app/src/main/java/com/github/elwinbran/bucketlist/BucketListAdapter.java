ff64d503712d9a9410b02193b18ddb38293466db
ElwinBran <elwinbran@gmail.com>
2018-12-16 21:47:47 +0100

    Added the adapter

---
+++ app/src/main/java/com/github/elwinbran/bucketlist/BucketListAdapter.java
@@ -0,0 +1,32 @@
+package com.github.elwinbran.bucketlist;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * The adapter for the bucket list element.
+ *
+ * @author Elwin Slokker
+ */
+public class BucketListAdapter extends RecyclerView.Adapter<ItemViewHolder>
+{
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i)
+    {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemViewHolder itemViewHolder, int i)
+    {
+
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return 0;
+    }
+}
