733665d9a72f0efa5517a554da46611303c2da46
ElwinBran <elwinbran@gmail.com>
2018-12-19 17:35:13 +0100

    Initial ROOM DAO

---
+++ app/src/main/java/com/github/elwinbran/bucketlist/BucketItemDAO.java
@@ -0,0 +1,29 @@
+package com.github.elwinbran.bucketlist;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+/**
+ *
+ * @author Elwin Slokker
+ */
+@Dao
+public interface BucketItemDAO
+{
+    @Query("SELECT * FROM bucketListItem")
+    public List<BucketListItem> getAllEntries();
+
+    @Insert
+    public void insertEntry(BucketListItem entry);
+
+    @Delete
+    public void deleteEntry(BucketListItem entry);
+
+    @Update
+    public void updateEntry(BucketListItem entry);
+}
