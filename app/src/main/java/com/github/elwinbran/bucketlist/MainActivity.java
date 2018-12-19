a1a8e569a8b121af0f958aa189e53e49785a1115
ElwinBran <elwinbran@gmail.com>
2018-12-19 17:34:15 +0100

    Started woring on implementing everything in the main class.

--- app/src/main/java/com/github/elwinbran/bucketlist/MainActivity.java
+++ app/src/main/java/com/github/elwinbran/bucketlist/MainActivity.java
@@ -1,9 +1,15 @@
 package com.github.elwinbran.bucketlist;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModel;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
 
 /**
  * The entry point of the app.
@@ -13,6 +19,8 @@ import android.support.v7.widget.RecyclerView;
  */
 public class MainActivity extends AppCompatActivity
 {
+
+    private List<BucketListItem> bucketItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -24,6 +32,24 @@ public class MainActivity extends AppCompatActivity
                 new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         BucketListAdapter adapter = new BucketListAdapter(null);
         bucketListView.setAdapter(adapter);
+
+        FloatingActionButton addItemButton = findViewById(R.id.add_item_button);
+        GenericRepository<BucketListItem> repository = null;
+        GenericCRUDViewModel<BucketListItem> viewModel = null;
+        viewModel.getReminders().observe(this, new Observer<List<BucketListItem>>()
+        {
+            @Override
+            public void onChanged(@Nullable List<BucketListItem> items)
+            {
+                bucketItems = items;
+                updateUI();
+            }
+        });
+
+    }
+
+    private void updateUI()
+    {
 
     }
 }
