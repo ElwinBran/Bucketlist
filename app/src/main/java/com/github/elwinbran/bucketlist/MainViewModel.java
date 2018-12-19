7a04b587d8d7bfdea9aec3ff5abdf949f57793cb
ElwinBran <elwinbran@gmail.com>
2018-12-19 18:23:54 +0100

    Added implementation for the view model

---
+++ app/src/main/java/com/github/elwinbran/bucketlist/MainViewModel.java
@@ -0,0 +1,47 @@
+package com.github.elwinbran.bucketlist;
+
+import android.arch.lifecycle.LiveData;
+
+import java.util.List;
+
+/**
+ *
+ * @author Elwin Slokker
+ */
+public class MainViewModel extends GenericCRUDViewModel<BucketListItem>
+{
+
+    final private GenericRepository<BucketListItem> repository;
+
+    final private LiveData<List<BucketListItem>> items;
+
+    public MainViewModel(GenericRepository<BucketListItem> repository, LiveData<List<BucketListItem>> items)
+    {
+        this.repository = repository;
+        this.items = items;
+    }
+
+    @Override
+    public LiveData<List<BucketListItem>> getModels()
+    {
+        return items;
+    }
+
+    @Override
+    public void insert(BucketListItem model)
+    {
+        this.repository.insert(model);
+    }
+
+    @Override
+    public void update(BucketListItem model)
+    {
+        this.repository.update(model);
+    }
+
+    @Override
+    public void delete(BucketListItem model)
+    {
+        this.repository.delete(model);
+    }
+}
