df1e7a90ca8e113700f2f2a03e69e64b145aeaad
ElwinBran <elwinbran@gmail.com>
2019-01-02 16:30:04 +0100

    Added the new activity which will allow for new bucketlist items (but is now empty)

---
+++ app/src/main/java/com/github/elwinbran/bucketlist/AddActivity.java
@@ -0,0 +1,18 @@
+package com.github.elwinbran.bucketlist;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ *
+ * @author Elwin Slokker
+ */
+public class AddActivity extends AppCompatActivity
+{
+    @Override
+    public void onCreate(Bundle savedInstances)
+    {
+        super.onCreate(savedInstances);
+        setContentView(R.layout.activity_add);
+    }
+}
