26bda03fa13bb66ad516ba16174af30672c2ada6
ElwinBran <elwinbran@gmail.com>
2019-01-02 15:58:41 +0100

    Added the property of making the text strikethrough on checking. DB functionality needs yet to be added.

--- app/src/main/java/com/github/elwinbran/bucketlist/ItemViewHolder.java
+++ app/src/main/java/com/github/elwinbran/bucketlist/ItemViewHolder.java
@@ -1,8 +1,11 @@
 package com.github.elwinbran.bucketlist;
 
+import android.graphics.Paint;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 /**
@@ -30,5 +33,32 @@ public class ItemViewHolder extends RecyclerView.ViewHolder
         completionChecker.setChecked(model.getCompleted());
         title.setText(model.getTitle());
         description.setText(model.getDescription());
+    }
+
+    private final CompoundButton.OnCheckedChangeListener statusChanged()
+    {
+        return new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b)
+            {
+                updateState(b);
+            }
+        };
+    }
+
+    private void updateState(Boolean newCompletionValue)
+    {
+        //TODO feedback to database
+        if(newCompletionValue)
+        {
+            title.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+            description.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+        }
+        else
+        {
+            title.setPaintFlags(0);
+            description.setPaintFlags(0);
+        }
     }
 }
